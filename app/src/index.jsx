f4ec568c8b84a5395d02d93eee0a90b6fa17ebb7
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-08 12:08:52 +0530

    The react and the electron app integration is working

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -4,4 +4,4 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("target");
 const root = createRoot(container);
 
-root.render(<React.Fragment>Hello world</React.Fragment>);
+root.render(<React.Fragment>Hello world and planet</React.Fragment>);
