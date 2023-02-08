7c902b8d8a066f418908edd99ee141fac672fad3
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-08 12:17:05 +0530

    Added the support for CSS and its working

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 const container = document.getElementById("target");
 const root = createRoot(container);
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const react_app_src = "app/src";
+const react_app_dist = "app/dist";
 
 module.exports = {
   entry: "./app/src/index.jsx",
   output: {
-    path: path.resolve(__dirname, "app/dist"),
+    path: path.resolve(__dirname, react_app_dist),
     filename: "bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        include: [path.resolve(__dirname, "app/src")],
+        include: [path.resolve(__dirname, react_app_src)],
         loader: "babel-loader",
         resolve: {
           extensions: [".js", ".jsx", ".json"]
@@ -19,10 +21,18 @@ module.exports = {
       },
       {
         test: /\.(html)$/,
-        include: [path.resolve(__dirname, "app/src")],
+        include: [path.resolve(__dirname, react_app_src)],
         use: {
           loader: "html-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, react_app_src)],
+        use: [
+            "style-loader",
+            "css-loader"
+        ]
       }
     ]
   },
