407ac12089aa4755e399964e21336282861ba6fd
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2023-02-08 11:54:48 +0530

    The react app bundling with our webpack is working

--- app/electron/main.js
+++ app/electron/main.js
@@ -3,12 +3,9 @@ const {
   BrowserWindow
 } = require("electron");
 
-// This method is called when Electron
-// has finished initializing
-app.whenReady().then(() => {
-
+function createWindow() {
   // Create a new window
-  let window = new BrowserWindow({
+  const window = new BrowserWindow({
     width: 800,
     height: 600,
     show: false
@@ -22,4 +19,27 @@ app.whenReady().then(() => {
 
   // Load our HTML file
   window.loadFile("index.html");
+}
+
+// This method is called when Electron
+// has finished initializing
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app/src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "app/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, "app/src")],
+        loader: "babel-loader",
+        resolve: {
+          extensions: [".js", ".jsx", ".json"]
+        }
+      }
+    ]
+  }
+}
